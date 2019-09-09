49a301ab3ea26d2b6d87008ca484dfe17e4da200
deterministic-forest <jwilli18@go.olemiss.edu>
2019-09-09 17:11:34 -0500

    Added Jinja templating to main script, changed data structure of output

---
+++ importfinder/__init__.py
@@ -0,0 +1,1 @@
+from .importfinder import ImportGraph
--- importfinder/importfinder.py
+++ importfinder/importfinder.py
@@ -1,58 +1,19 @@
 from pathlib import Path
-from collections import namedtuple, defaultdict
-import tokenize
-from typing import List, Dict, Type
+from collections import namedtuple
+from typing import List
 import networkx as nx
 import ast
-#TODO: Make sure that venvs are not visited
-
-def get_all_py_files() -> List[Path]:
-    """Recursively walks down cwd, finds all .py files"""
-    p = Path.cwd()
-    return list(p.glob('**/*.py'))
-
-
-def tokenizer(filepath: Path) -> List[namedtuple]:
-    """Tokenizes *source* and returns the tokens as a list of lists."""
-    return [a for a in tokenize.generate_tokens(open(filepath).readline)]
-
-
-def enumerate_imports(tokens: List[namedtuple], local_pkgs: List[str]):
-    """
-    Iterates over *tokens* and returns a list of all imported modules.
-    Since only local imports are being analyzed, try/except logic is excluded
-    **Note:** This is intelligent about the use of the 'as' keyword.
-    TODO: Account for imports in parentheses
-    TODO: Account for multiple periods
-    """
-    imported_modules = []
-    import_line = False
-    for index, tok in enumerate(tokens):
-        if tok.type == tokenize.NEWLINE:
-            import_line = False
-        elif tok.string == "import":
-            import_line = True
-        elif import_line:
-            next_tok = tokens[index+1]
-            if tok.type == tokenize.NAME and next_tok.string != 'as':
-                if next_tok.string not in imported_modules:
-                    if next_tok.string in local_pkgs:
-                        print('added import')
-                        imported_modules.append(next_tok.string)
-                    if next_tok.string == '.':
-                        print('added import')
-                        imported_modules.append(tokens[index+2].string)
-
-    return imported_modules
+from jinja2 import Environment, FileSystemLoader
+# TODO: Make sure that venvs are not visited
 
 
 class ImportGraph(object):
 
-    def __init__(self):
+    def __init__(self, directory=Path.cwd()):
         self.G = nx.Graph()
-        self.directory = Path.cwd()
+        self.directory = directory
         self.local_libs = set(self.packages + self.modules)
-        self.Import = namedtuple('Import', 'module name')
+        self.Import = namedtuple('Import', 'nodes links parents')
 
     @property
     def _package_paths(self) -> List[Path]:
@@ -92,18 +53,15 @@ class ImportGraph(object):
 
         for node in ast.iter_child_nodes(root):
             if isinstance(node, ast.Import):
-                module = node.module.split('.')
-                intersect = set(module) & self.local_libs
-                if intersect:
-                    e = [(py_file.name, i) for i in intersect]
-                    self.G.add_edges_from(*e)
+                continue
 
             elif isinstance(node, ast.ImportFrom):
                 module = node.module.split('.')
                 intersect = set(module) & self.local_libs
                 if intersect:
-                    e = [(py_file.name, i) for i in intersect]
-                    self.G.add_edges_from(*e)
+                    for i in intersect:
+                        self.G.add_edge(py_file.stem, i)
+
             else:
                 continue
 
@@ -111,34 +69,33 @@ class ImportGraph(object):
                 name = n.name.split('.')
                 intersect = set(name) & self.local_libs
                 if intersect:
-                    e = [(py_file.name, i) for i in intersect]
-                    self.G.add_edges_from(*e)
+                    for i in intersect:
+                        self.G.add_edge(py_file.stem, i)
 
     def _construct_graph(self):
         for py_file in self._module_paths:
             self._get_imports(py_file)
 
     def output_graph(self):
-        return nx.to_dict_of_dicts(self.G)
+        self._construct_graph()
+        nodes, links, parents = [], [], []
+        for node in self.G.nodes(data=True):
+            for edge in self.G[node[0]]:
+                try:
+                    nodes.append(node[0])
+                    links.append(edge)
+                    parents.append(node[1]['package'])
+                except KeyError:
+                    #takes care of base module imports
+                    pass
+        return nodes, links, parents
 
-def get_imports(modules: Dict[str, List[Path]]) -> List[namedtuple]:
-    links = []
-
-    for pkg, mods in modules.items():
-        for mod in mods:
-            for statement in enumerate_imports(tokenizer(mod),
-                                               list(modules.keys())):
-                links.append(link._make([pkg, mod.stem, statement]))
-    return links
-
-
-def main():
-    packages = get_all_packages()
-    assert len(packages) > 0, "No packages found, double-check directory"
-    modules = get_all_modules(packages)
-    return get_imports(modules)
 
 
 if __name__ == '__main__':
-    print(main())
-
+    test = ImportGraph(directory=Path('/home/dal/PycharmProjects/pyjanitor_fork')).output_graph()
+    env = Environment(loader=FileSystemLoader('../templates'))
+    template = env.get_template('hive.html')
+    output = template.render(nodes=test[0], links=test[1], parents=test[2])
+    with open('test_graph.html', 'w') as f:
+        f.write(output)
